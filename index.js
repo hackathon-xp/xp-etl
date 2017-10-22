149e1cdce5249bc980ddba9a6e792796d9280ad0
Erick Wendel <erickwendel@Ericks-MacBook-Pro.local>
2017-10-22 02:59:58 -0200

    v1

--- index.js
+++ index.js
@@ -26,27 +26,35 @@ async function remove(params) {
 }
 async function run() {
   await remove();
-  if (fs.existsSync('my.json')) fs.unlinkSync('my.json');
+  if (fs.existsSync('my.csv')) fs.unlinkSync('my.csv');
 
   console.log(`base limpa`);
   console.log('obtendo informações...');
   csv()
     .fromFile(csvFilePath)
-    .on('json', jsonObj => {
-      let data = [];
-      if (fs.existsSync('my.json')) {
-        data = JSON.parse(fs.readFileSync('my.json'));
-      }
-      data.push(jsonObj);
-      fs.writeFileSync('my.json', JSON.stringify(data));
+    .on('json', async jsonObj => {
+      // let data = null;
+      // if (fs.existsSync('my.csv')) {
+      //   data = fs.readFileSync('my.csv');
+      // } else {
+      //   data = '';
+      //   fs.writeFileSync('my.csv', '');
+      // }
+      // const { field1 } = jsonObj;
+      // if (data.indexOf(field1) !== -1) {
+      //   return;
+      // }
+      console.log('inserindo...', jsonObj.field1);
+      await insert(jsonObj);
+      // fs.appendFileSync('my.csv', `${field1}\n`);
     })
     .on('done', async error => {
-      console.log('inserindo items...');
-      const items = JSON.parse(fs.readFileSync('my.json'));
-      const insertItem = await Promise.all(items.map(item => insert(item)));
-      console.log('inserts', insertItem.length);
+      // console.log('inserindo items...');
+      // const items = JSON.parse(fs.readFileSync('my.csv'));
+      // const insertItem = await Promise.all(items.map(item => insert(item)));
+      // console.log('inserts', insertItem.length);
       console.log('end');
-      process.exit(0);
+      // process.exit(0);
     });
 }
 
